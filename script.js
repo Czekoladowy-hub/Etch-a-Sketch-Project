9b4a75eb093bdbd135197326ba1cdf04c31826fb
Dominik <kontakt.minik@gmail.com>
2023-04-05 16:24:10 +0200

    Add comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.onload = createSheet;
 const mainContainer = document.querySelector("#mainContainer");
 
+// Take user input about how much squares per site he wants
 let squaresPerSite = +prompt("Squares: ");
 
+// Function to create whole sheet
 function createSheet() {
+  // Take div container width and devide it by user input to calculate space for 1 square
   let squareWidth = mainContainer.clientWidth / squaresPerSite;
 
   for (let i = 1; i <= squaresPerSite * squaresPerSite; i++) {
@@ -11,6 +14,7 @@ function createSheet() {
     artDiv.classList.toggle("divArt");
     artDiv.style.width = squareWidth + "px";
     artDiv.innerHTML = i;
+    // Add listeners to control if cursore is hovering div and change color
     artDiv.addEventListener("mouseover", () => {
       artDiv.style.background = "blue";
     });
@@ -21,6 +25,5 @@ function createSheet() {
   }
   console.log(squareWidth);
   console.log(mainContainer.clientWidth);
-
   console.log(squaresPerSite);
 }

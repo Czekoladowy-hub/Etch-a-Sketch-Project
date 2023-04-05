16d4decdef2fc429477213c9219603c587b440f9
Dominik <kontakt.minik@gmail.com>
2023-04-05 19:38:38 +0200

    Create part of function removing sheet

--- script.js
+++ script.js
@@ -16,22 +16,26 @@ for (let i = 1; i <= 256; i++) {
   mainContainer.appendChild(artDiv);
 }
 
-function createCustomSheet() {
-  for (let i = 1; i <= squaresPerSite * squaresPerSite; i++) {
-    const artDiv = document.createElement("div");
-    artDiv.classList.toggle("divArt");
-    artDiv.style.width = squareWidth + "px";
-    artDiv.innerHTML = i;
-
-    artDiv.addEventListener("mouseover", () => {
-      artDiv.style.background = "blue";
-    });
-    artDiv.addEventListener("mouseout", () => {
-      artDiv.style.background = "white";
-    });
-    mainContainer.appendChild(artDiv);
-  }
+function removeSheet() {
+  const divArt = document.querySelectorAll(".divArt");
+  mainContainer.removeChild(divArt);
 }
+
+// for (let i = 1; i <= squaresPerSite * squaresPerSite; i++) {
+//   // const artDiv = document.createElement("div");
+//   // artDiv.classList.toggle("divArt");
+//   // artDiv.style.width = squareWidth + "px";
+//   // artDiv.innerHTML = i;
+
+//   // artDiv.addEventListener("mouseover", () => {
+//   //   artDiv.style.background = "blue";
+//   // });
+//   // artDiv.addEventListener("mouseout", () => {
+//   //   artDiv.style.background = "white";
+//   // });
+//   // mainContainer.appendChild(artDiv);
+// }
+//}
 
 // function userInput() {
 //   let squaresPerSite = +prompt("Squares: ");

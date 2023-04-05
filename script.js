bb91364921f80044870ec1bec66f0539bae116a7
Dominik <kontakt.minik@gmail.com>
2023-04-05 19:04:12 +0200

    Move for loop creating basic sheet to global scope

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-window.onload = createSheet;
 const mainContainer = document.querySelector("#mainContainer");
+let squaresPerSite = 16;
+let squareWidth = mainContainer.clientWidth / squaresPerSite;
 
-// Take user input about how much squares per site he wants
-let squaresPerSite = +prompt("Squares: ");
+for (let i = 1; i <= 256; i++) {
+  const artDiv = document.createElement("div");
+  artDiv.classList.toggle("divArt");
+  artDiv.style.width = squareWidth + "px";
+  artDiv.innerHTML = i;
+  artDiv.addEventListener("mouseover", () => {
+    artDiv.style.background = "blue";
+  });
+  artDiv.addEventListener("mouseout", () => {
+    artDiv.style.background = "white";
+  });
+  mainContainer.appendChild(artDiv);
+}
 
-// Function to create whole sheet
-function createSheet() {
-  // Take div container width and devide it by user input to calculate space for 1 square
-  let squareWidth = mainContainer.clientWidth / squaresPerSite;
-
+function createCustomSheet() {
   for (let i = 1; i <= squaresPerSite * squaresPerSite; i++) {
     const artDiv = document.createElement("div");
     artDiv.classList.toggle("divArt");
     artDiv.style.width = squareWidth + "px";
     artDiv.innerHTML = i;
-    // Add listeners to control if cursore is hovering div and change color
+
     artDiv.addEventListener("mouseover", () => {
       artDiv.style.background = "blue";
     });
@@ -23,7 +31,9 @@ function createSheet() {
     });
     mainContainer.appendChild(artDiv);
   }
-  console.log(squareWidth);
-  console.log(mainContainer.clientWidth);
-  console.log(squaresPerSite);
 }
+
+// function userInput() {
+//   let squaresPerSite = +prompt("Squares: ");
+//   mainContainer.removeChild(artDiv);
+// }
